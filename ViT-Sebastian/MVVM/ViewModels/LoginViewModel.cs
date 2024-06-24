ee060b1f6e48f0b153561a0c58ee9c09485163da
Илья Лимонов <JIuMoHkA99@mail.ru>
2024-06-24 10:32:56 +0400

    Добавлено: Фронт -  Страница Авторизации

---
+++ ViT-Sebastian/MVVM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ViT_Sebastian.MVVM.ViewModels
+{
+    partial class LoginViewModel : ObservableObject
+    {
+        [RelayCommand]
+        void Login()
+        {
+            // ОБРАЩЕНИЕ К БД
+            // ВЗЯТЬ ИЗ БД ПО ТАБЕЛЬНОМУ НОМЕРУ ЗАПИСЬ И СРАВНИТЬ ПАРОЛЬ
+            // ЕСЛИ ПАРОЛЬ ВЕРНЫЙ - ПУСТИТЬ В СИСТЕМУ ПЕРЕДАВ ТАБЕЛЬНЫЙ НОМЕР
+        }
+    }
+}
---
+++ ViT-Sebastian/MVVM/Views/LoginView.xaml.cs
@@ -0,0 +1,9 @@
+namespace ViT_Sebastian.MVVM.Views;
+
+public partial class LoginView : ContentPage
+{
+	public LoginView()
+	{
+		InitializeComponent();
+	}
+}
